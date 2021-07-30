159c90be667a80d36e66ff8c3741576b665da97d
ABZZDEV <42191357+Abzzdev@users.noreply.github.com>
2021-07-30 18:23:08 +0100

    Update Statistics.jsx

--- React/CoTeach/src/components/Statistics.jsx
+++ React/CoTeach/src/components/Statistics.jsx
@@ -1,31 +1,125 @@
 import React, { Component } from "react";
-import { Statistics } from ".";
+import 'antd/dist/antd.css';
+import '../index.css';
+import { Statistic, Card, Row, Col, Layout } from 'antd';
+import { ArrowUpOutlined, ArrowDownOutlined, LikeOutlined } from '@ant-design/icons';
+
+const { Content, Footer, Sider } = Layout;
 
 class StatisticsPage extends Component {
     render() {
         return (
-            <div className="home">
-                <div class="container">
-                    <div class="row align-items-center my-5">
-                        <div class="col-lg-7">
-                            <img
-                                class="img-fluid rounded mb-4 mb-lg-0"
-                                src=""
-                                alt=""
-                            />
+            <Layout>
+                <Sider className="sidebar">
+                    <p></p>
+                </Sider>
+                <Layout className="site-layout">
+                    <Content className="addLesson-content">
+                        <div className="site-layout-background">
+                            <div style={{ padding: "0% 5% 100%" }}>
+                                <h1>Statistics</h1>
+                                <div className="site-statistic-demo-card">
+                                    <Row gutter={16}>
+                                        <Col span={6}>
+                                            <Card>
+                                                <Statistic
+                                                    title="Active"
+                                                    value={11.28}
+                                                    precision={2}
+                                                    valueStyle={{ color: '#3f8600' }}
+                                                    prefix={<ArrowUpOutlined />}
+                                                    suffix="%"
+                                                />
+                                            </Card>
+                                        </Col>
+                                        <Col span={6}>
+                                            <Card>
+                                                <Statistic
+                                                    title="Idle"
+                                                    value={9.3}
+                                                    precision={2}
+                                                    valueStyle={{ color: '#cf1322' }}
+                                                    prefix={<ArrowDownOutlined />}
+                                                    suffix="%"
+                                                />
+                                            </Card>
+                                        </Col>
+                                        <Col span={6}>
+                                            <Card>
+                                                <Statistic
+                                                    title="Active"
+                                                    value={11.28}
+                                                    precision={2}
+                                                    valueStyle={{ color: '#3f8600' }}
+                                                    prefix={<ArrowUpOutlined />}
+                                                    suffix="%"
+                                                />
+                                            </Card>
+                                        </Col>
+                                        <Col span={6}>
+                                            <Card>
+                                                <Statistic
+                                                    title="Idle"
+                                                    value={9.3}
+                                                    precision={2}
+                                                    valueStyle={{ color: '#cf1322' }}
+                                                    prefix={<ArrowDownOutlined />}
+                                                    suffix="%"
+                                                />
+                                            </Card>
+                                        </Col>
+                                    </Row>
+                                    <br/>
+                                    <Row gutter={16}>
+                                    <Col span={6}>
+                                            <Card>
+                                                <Statistic
+                                                    title="Active"
+                                                    value={11.28}
+                                                    precision={2}
+                                                    valueStyle={{ color: '#3f8600' }}
+                                                    prefix={<ArrowUpOutlined />}
+                                                    suffix="%"
+                                                />
+                                            </Card>
+                                        </Col>
+                                        <Col span={6}>
+                                            <Card>
+                                                <Statistic
+                                                    title="Idle"
+                                                    value={9.3}
+                                                    precision={2}
+                                                    valueStyle={{ color: '#cf1322' }}
+                                                    prefix={<ArrowDownOutlined />}
+                                                    suffix="%"
+                                                />
+                                            </Card>
+                                        </Col>
+                                        <Col span={6}>
+                                            <Card>
+                                                <Statistic
+                                                    title="Active"
+                                                    value={11.28}
+                                                    precision={2}
+                                                    valueStyle={{ color: '#3f8600' }}
+                                                    prefix={<ArrowUpOutlined />}
+                                                    suffix="%"
+                                                />
+                                            </Card>
+                                        </Col>
+                                        <Col span={6}>
+                                            <Card>
+                                                <Statistic title="Feedback" value={1128} prefix={<LikeOutlined />} />
+                                            </Card>
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-lg-5">
-                            <h1 class="font-weight-light">Statistics</h1>
-                            <p>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting
-                                industry. Lorem Ipsum has been the industry's standard dummy text
-                                ever since the 1500s, when an unknown printer took a galley of
-                                type and scrambled it to make a type specimen book.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>ALET CoTeach 2021</Footer>
+                </Layout>
+            </Layout>
         );
     };
 }
