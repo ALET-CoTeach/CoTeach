63a47f3b7fbc0448844abf10fd58a629be3e2bc4
Sam <sam@squail.org>
2021-08-02 16:12:35 +0100

    Added Lesson Request model

---
+++ server/api/v1/models/LessonRequest.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const LessonRequestSchema = new Schema(
+    {
+        schoolid: {
+            type: String,
+            require: true,
+        },
+        year: {
+            type: Number,
+            require: true,
+        },
+        subject: {
+            type: String,
+            require: true,
+        },
+        LessonTitle: {
+            type: String,
+            require: true,
+        },
+        LessonDetails: {
+            type: String,
+            require: true,
+        },
+        term: {
+            
+        },
+        preferredDay: {
+            type: String,
+            require: true,
+        },
+        preferredTime: {
+            type: String,
+            require: true
+        },
+        teacherId: {
+            type: String,
+            require: true,
+        }
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const LessonRequest = mongoose.model('LessonRequest',LessonRequestSchema );
+
+module.exports = LessonRequest;
