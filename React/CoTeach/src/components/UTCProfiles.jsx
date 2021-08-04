3235669f4df64a2223542d6b4298a030b32caa5d
ABZZDEV <42191357+Abzzdev@users.noreply.github.com>
2021-08-04 18:12:41 +0100

    Update UTCProfiles.jsx

--- React/CoTeach/src/components/UTCProfiles.jsx
+++ React/CoTeach/src/components/UTCProfiles.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import 'antd/dist/antd.css';
+import { Layout, Collapse } from 'antd';
+
+const { Content, Footer, Sider } = Layout;
+
+const { Panel } = Collapse;
 
 class UTCProfiles extends React.Component {
     render() {
-        return(
-            <div>
-                <h1>This is the UTC Profiles Page</h1>
-            </div>
+        return (
+            <Layout>
+                <Sider className="sidebar">
+                    <AddLessonModal />
+                </Sider>
+                <Layout className="site-layout">
+                    <Content className="addLesson-content">
+                        <div className="site-layout-background">
+                            
+                            <div>CONTENT HERE</div>
+
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>ALET CoTeach 2021</Footer>
+                </Layout>
+            </Layout>
         );
     }
 }
