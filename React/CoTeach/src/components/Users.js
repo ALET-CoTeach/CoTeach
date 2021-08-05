428b67f6e23c614fc875df806cb5cb014ef2dd77
ABZZDEV <42191357+Abzzdev@users.noreply.github.com>
2021-08-05 14:34:53 +0100

    Update Users.js

--- React/CoTeach/src/components/Users.js
+++ React/CoTeach/src/components/Users.js
@@ -1,10 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, Component } from "react";
+import axios from "axios";
 
+//const baseURL = "/api/v1/users";
+
+/*
 function Users() {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        fetch("/users/").then(res => {
+        fetch("api/v1/users/").then(res => {
             if (res.ok) {
                 return res.json()
             }
@@ -13,9 +17,32 @@ function Users() {
 
     return(
         <div>
+            <h1 style={{ textAlign: "center", }}> Users From Test Backend </h1>
             {users.map(user => <li>{user}</li>)}
         </div>
     )
+}
+
+{users.map(user => <li>{user}</li>)}
+*/
+
+function Users() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/v1/users/').then((response) => {
+            setUsers(response.data);
+        });
+    }, []);
+
+    //if (!users) return null;
+
+    return(
+        <div>
+            <h1 style={{ textAlign: "center", }}> Users From Test Backend </h1>
+            <div>{users.body}</div>
+        </div>
+    );
 }
 
 export default Users;
