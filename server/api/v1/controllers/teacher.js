bc77719a38d02f14f71f5eecfbaa71569378fb3d
samsonnagamani <samson.nagamani@pm.me>
2021-08-08 16:07:34 +0100

    Change findOneByIdAndDelete to findByIdAndDelete

--- server/api/v1/controllers/teacher.js
+++ server/api/v1/controllers/teacher.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 module.exports.deleteOne = (teacherId) =>
   new Promise(async (resolve, reject) => {
     try {
-      const teacher = await Teacher.findOneByIdAndDelete(teacherId);
+      const teacher = await Teacher.findByIdAndDelete(teacherId);
       
       if (!teacher) {
         resolve({ message: "Teacher document never existed or has already been deleted" }); 
