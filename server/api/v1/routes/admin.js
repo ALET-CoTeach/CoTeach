b6ee2e77131e2e41d5d68e0957d42ec8c7120f1e
samsonnagamani <samson.nagamani@pm.me>
2021-08-08 22:45:14 +0100

    Formatted code

--- server/api/v1/routes/admin.js
+++ server/api/v1/routes/admin.js
@@ -9,15 +9,15 @@ router.post('/signin', AdminController.access);
 router.post('/signout', requiredRoles(['admin']), AdminController.deauth);
 
 router.delete('/:adminId', requiredRoles(['admin']), (req, res) => {
-    const { adminId } = req.params;
+  const { adminId } = req.params;
 
-    AdminController.deleteOne(adminId)
-        .then((jsonResponse) => {
-            res.status(200).json(jsonResponse);
-        })
-        .catch((err) => {
-            res.status(500).json({ error: err });
-        });
+  AdminController.deleteOne(adminId)
+    .then((jsonResponse) => {
+      res.status(200).json(jsonResponse);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
 });
 
 router.put('/:adminId', requiredRoles(['admin']), (req, res) => {
@@ -33,4 +33,5 @@ router.put('/:adminId', requiredRoles(['admin']), (req, res) => {
             res.status(500).json({ error: err });
         });
 });
+
 module.exports = router;
--- server/api/v1/routes/slt.js
+++ server/api/v1/routes/slt.js
@@ -9,29 +9,30 @@ router.post('/signin', SLTController.access);
 router.post('/signout', requiredRoles(['slt']), SLTController.deauth);
 
 router.delete('/:sltId', requiredRoles(['admin']), (req, res) => {
-    const { sltId } = req.params;
+  const { sltId } = req.params;
 
-    SLTController.deleteOne(sltId)
-        .then((jsonResponse) => {
-            res.status(200).json(jsonResponse);
-        })
-        .catch((err) => {
-            res.status(500).json({ error: err });
-        });
+  SLTController.deleteOne(sltId)
+    .then((jsonResponse) => {
+      res.status(200).json(jsonResponse);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
 });
 
 router.put('/:sltId', requiredRoles(['admin']), (req, res) => {
-    const { sltId } = req.params;
+  const { sltId } = req.params;
 
-    const updateData = req.body;
+  const updateData = req.body;
 
-    TeacherController.updateOne(sltId, updateData)
-        .then((jsonResponse) => {
-            res.status(500).json(jsonResponse);
-        })
-        .catch((err) => {
-            res.status(500).json({ error: err });
-        });
+  TeacherController.updateOne(sltId, updateData)
+    .then((jsonResponse) => {
+      res.status(500).json(jsonResponse);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
 });
+
 module.exports = router;
 
