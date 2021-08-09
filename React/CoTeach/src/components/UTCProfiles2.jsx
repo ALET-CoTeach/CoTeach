f45028083df189c5254fd65baef538b440abcdaf
Nicola Harrison <nicola@harrisonsworld.com>
2021-08-09 21:37:02 +0100

    UTC Profile Page Complete

--- React/CoTeach/src/components/UTCProfiles2.jsx
+++ React/CoTeach/src/components/UTCProfiles2.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css';
 import { Layout, Collapse, Drawer, List, Avatar, Divider, Col, Row, Card } from 'antd';
+import '../components/loginPage.css';
 
 import UTCHeathrowLogo from 'url:~/src/assets/BlueLogos/UTC-Heathrow-Logo.svg';
 import BicesterTechnology from 'url:~/src/assets/BlueLogos/Bicester-Tchnology-Studio-Logo.svg';
@@ -10,6 +11,13 @@ import UTCOxfordshire from 'url:~/src/assets/BlueLogos/UTC-Oxfordshire-Logo.svg'
 import UTCReading from 'url:~/src/assets/BlueLogos/UTC-Reading-Logo.svg';
 import UTCSwindon from 'url:~/src/assets/BlueLogos/UTC-Swindon-Logo.svg';
 
+import UTCReadingBuilding from 'url:~/src/assets/SchoolPhotos/utc-reading.jpg';
+import UTCSwindonBuidling from 'url:~/src/assets/SchoolPhotos/utc-swindon-building.jpg';
+import UTCHeathrowBuilding from 'url:~/src/assets/SchoolPhotos/utc-heathrow-building.jpg';
+import UTCOxfordBuilding from 'url:~/src/assets/SchoolPhotos/utc-oxford-building.jpg';
+import ThealeGreenBuilding from 'url:~/src/assets/SchoolPhotos/theale-green-building.png';
+import BicesterBuilding from 'url:~/src/assets/SchoolPhotos/bicester-building.jpg';
+import BicesterStudioBuilding from 'url:~/src/assets/SchoolPhotos/bicester-technology-building.jpg';
 
 const { Content, Footer, Sider } = Layout;
 
@@ -24,24 +32,41 @@ const DescriptionItem = ({ title, content }) => (
 
 const data = [
     {
-      title: 'Title 1',
+        title: 'UTC Reading',
+        imageURL: UTCReadingBuilding,
+        schoolLogo: UTCReading,
     },
     {
-      title: 'Title 2',
+        title: 'UTC Swindon',
+        imageURL: UTCSwindonBuidling,
+        schoolLogo: UTCSwindon,
     },
     {
-      title: 'Title 3',
+        title: 'UTC Heathrow',
+        imageURL: UTCHeathrowBuilding,
+        schoolLogo: UTCHeathrowLogo,
     },
     {
-      title: 'Title 4',
+        title: 'UTC Oxford',
+        imageURL: UTCOxfordBuilding,
+        schoolLogo: UTCOxfordshire,
     },
     {
-      title: 'Title 5',
+        title: 'Theale Green',
+        imageURL: ThealeGreenBuilding,
+        schoolLogo: ThealeGreen,
     },
     {
-      title: 'Title 6',
+        title: 'Bicester',
+        imageURL: BicesterBuilding,
+        schoolLogo: Bicester,
     },
-  ];
+    {
+        title: 'Bicester Technology Studio',
+        imageURL: BicesterStudioBuilding,
+        schoolLogo: BicesterTechnology,
+    },
+];
 
 
 class UTCProfilesTwo extends React.Component {
@@ -62,105 +87,109 @@ class UTCProfilesTwo extends React.Component {
     render() {
         const schoolProfiles = {}
         return (
-            <Layout>
-                <Sider className="sidebar">
-                </Sider>
-                <Layout className="site-layout">
-                    <Content className="addLesson-content">
-                        <div className="site-layout-background">
+            <div >
+                <Layout style={{ height: "100%" }}>
+                    <Sider className="sidebar">
+                    </Sider>
+                    <Layout className="site-layout">
+                        <Content className="addLesson-content" width="100%">
+                            <div className="site-layout-background">
 
-                            <div>
-                                <>
-                                    <List
-                                        grid={{
-                                            gutter: 16,
-                                            xs: 1,
-                                            sm: 2,
-                                            md: 4,
-                                            lg: 4,
-                                            xl: 6,
-                                            xxl: 3,
-                                        }}
-                                        dataSource={data}
-                                        renderItem={item => (
-                                            <List.Item
-                                            
-                                            >
-                                                <Card 
-                                                title={item.title}
-                                                key={item.id}
-                                                actions={[
-                                                    <a onClick={this.showDrawer} key={`a-${item.id}`}>
-                                                        View Profile
-                                                    </a>,
-                                                ]}
-                                                >Card content</Card>
-                                            </List.Item>
-                                        )}
-                                    />
-                                    <Drawer
-                                        width={640}
-                                        placement="right"
-                                        closable={false}
-                                        onClose={this.onClose}
-                                        visible={this.state.visible}
-                                    >
+                                <div style={{ height: "100%" }}>
+                                    <>
+                                        <List
+                                            grid={{
+                                                gutter: 16,
+                                                xs: 1,
+                                                sm: 2,
+                                                md: 4,
+                                                lg: 4,
+                                                xl: 6,
+                                                xxl: 7, //used to be 3
+                                            }}
+                                            dataSource={data}
+                                            renderItem={item => (
+                                                <List.Item
+
+                                                >
+                                                    <Card
+                                                        title={item.title}
+                                                        key={item.id}
+                                                        actions={[
+                                                            <a onClick={this.showDrawer} key={`a-${item.id}`}>
+                                                                Coordinator Information
+                                                            </a>,
+                                                        ]}
+                                                    >
+                                                        <img src={item.schoolLogo} width="100%" />
+                                                    </Card>
+                                                </List.Item>
+                                            )}
+                                        />
+                                        <Drawer
+                                            width={640}
+                                            placement="right"
+                                            closable={false}
+                                            onClose={this.onClose}
+                                            visible={this.state.visible}
+                                        >
 
 
-                                        <h1 className="site-description-item-profile-p">UTC Reading</h1>
-                                        <br />
-                                        <Row>
-                                            <Col span={8}>
-                                                <DescriptionItem
-                                                    title="School Website"
-                                                    content={
-                                                        <a href="https://www.utcreading.co.uk/">
-                                                            utcreading.co.uk
-                                                        </a>
-                                                    }
-                                                />
-                                            </Col>
-                                            <Col span={8}>
-                                                <DescriptionItem title="Coordinator Name" content="Amy Sutcliffe" />
-                                            </Col>
+                                            <h1 className="site-description-item-profile-p">UTC Reading</h1>
+                                            <br />
+                                            <Row>
+                                                <Col span={8}>
+                                                    <DescriptionItem
+                                                        title="School Website"
+                                                        content={
+                                                            <a href="https://www.utcreading.co.uk/">
+                                                                utcreading.co.uk
+                                                            </a>
+                                                        }
+                                                    />
+                                                </Col>
+                                                <Col span={8}>
+                                                    <DescriptionItem title="Coordinator Name" content="Amy Sutcliffe" />
+                                                </Col>
 
-                                            <Col span={8}>
-                                                <DescriptionItem title="School Address" content="Crescent Rd, Reading RG1 5RQ" />
-                                            </Col>
-                                        </Row>
+                                                <Col span={8}>
+                                                    <DescriptionItem title="School Address" content="Crescent Rd, Reading RG1 5RQ" />
+                                                </Col>
+                                            </Row>
 
-                                        <br /><hr /><br />
-                                        <h2 className="site-description-item-profile-p">Contacts</h2><br />
-                                        <Row>
-                                            <Col span={12}>
-                                                <DescriptionItem title="Email" content="AntDesign@example.com" />
-                                            </Col>
-                                            <Col span={12}>
-                                                <DescriptionItem title="Phone Number" content="+86 181 0000 0000" />
-                                            </Col>
-                                        </Row>
-                                        <br /><hr /><br />
-                                        <h2 className="site-description-item-profile-p">Coordinator Contacts</h2><br />
-                                        <Row>
-                                            <Col span={12}>
-                                                <DescriptionItem title="Email" content="AntDesign@example.com" />
-                                            </Col>
-                                            <Col span={12}>
-                                                <DescriptionItem title="Phone Number" content="+86 181 0000 0000" />
-                                            </Col>
-                                        </Row>
-                                        <br /><hr /><br />
+                                            <br /><hr /><br />
+                                            <h2 className="site-description-item-profile-p">Contacts</h2><br />
+                                            <Row>
+                                                <Col span={12}>
+                                                    <DescriptionItem title="Email" content="AntDesign@example.com" />
+                                                </Col>
+                                                <Col span={12}>
+                                                    <DescriptionItem title="Phone Number" content="+86 181 0000 0000" />
+                                                </Col>
+                                            </Row>
+                                            <br /><hr /><br />
+                                            <h2 className="site-description-item-profile-p">Coordinator Contacts</h2><br />
+                                            <Row>
+                                                <Col span={12}>
+                                                    <DescriptionItem title="Email" content="AntDesign@example.com" />
+                                                </Col>
+                                                <Col span={12}>
+                                                    <DescriptionItem title="Phone Number" content="+86 181 0000 0000" />
+                                                </Col>
+                                            </Row>
+                                            <br /><hr /><br />
 
-                                    </Drawer>
-                                </>
+                                        </Drawer>
+                                    </>
+
+                                </div>
 
                             </div>
-
-                        </div>
-                    </Content>
-                    <Footer style={{ textAlign: 'center' }}>ALET CoTeach 2021</Footer>
-                </Layout>
-            </Layout >
+                        </Content>
+                        <Footer style={{ textAlign: 'center' }}>ALET CoTeach 2021</Footer>
+                    </Layout>
+                </Layout >
+            </div>
         );
     }
 }
