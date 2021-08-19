ea6209fb1b7791812bf60e48437968d1fd1f67f6
samsonnagamani <samson.nagamani@pm.me>
2021-08-19 17:30:50 +0100

    Converted to functional React components

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-const About = () => {
-    return (
-        <div>
-            About
-        </div>
-    )
-}
+const About = () => (
+  <div>
+    About
+  </div>
+);
 
-export default About
+export default About;
--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -1,80 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './loginPage.css';
 import 'antd/dist/antd.css';
-import { Card, Image, Form, Input, Button, Checkbox, Radio, Row, Col } from 'antd';
+import {
+  Card, Image, Form, Input, Button, Checkbox, Radio, Row, Col,
+} from 'antd';
 import coTeach from '@assets/CoTeach-Logo-Blue.svg';
-import LoginTabs from './Tabs/LoginTabs';
 import axios from 'axios';
+import LoginTabs from './Tabs/LoginTabs';
 
 const validateMessages = {
-    required: '${label} is required!',
-    types: {
-        email: 'This is not a valid email!',
-    },
+  required: '${label} is required!',
+  types: {
+    email: 'This is not a valid email!',
+  },
 };
 
 // const BASE_URI = process.env.BASE_API_URI;
+const BASE_URI = 'http://localhost:5001/api/v1';
 
-class LoginPage extends React.Component {
+const LoginPage = () => {
+  const [authLevel, setAuthLevel] = useState('teacher');
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            authLevel: "teacher"
-        };
-    }
+  const changeAuthLevel = (value) => {
+    setAuthLevel({ authLevel: value });
+  };
 
-    changeAuthLevel = (value) => {
-        this.setState({ authLevel: value });
-    }
+  const handleSubmit = async (values) => {
+    const { email, password } = values;
+    const res = await axios.post(`${BASE_URI}/${authLevel}/signin`, {
+      email,
+      password,
+    });
 
-    handleSubmit = async (values) => {
-        const { email, password } = values;
-        const res = await axios.post(`${BASE_URI}/${this.state.authLevel}/signin`, {
-            email,
-            password,
-        });
+    console.log(authLevel);
+    console.log(res);
+  };
 
-        console.log(res);
-    }
-
-
-    render() {
-        return (
-            <div className="LoginBody">
-                <Row type="flex" align="bottom">
-                    <Col className="gutter-row" lg={2} sm={24}
-                    />
-                    <Col className="gutter-row" lg={8} sm={24} xs={24}>
-                        <div className="cardAlignment cardPadding">
-                            <Card className="cardStyling" style={{ boxShadow: "5px 8px 24px 5px rgba(0, 0, 0, 0.2)" }}>
-                                <h1 className="centerText">CoTeach Sign In</h1>
-                                <div align="center">
-                                    <img src={coTeach} width="30%" />
-                                </div>
-                                <Form
-                                    name="basic"
-                                    type="flex"
-                                    justify="center"
-                                    align="middle"
-                                    validateMessages={validateMessages}
-                                    initialValues={{
-                                        remember: true
-                                    }}
-                                    onFinish={this.handleSubmit}
-                                >
-                                    <LoginTabs changeAuthLevel={this.changeAuthLevel} />
-                                </Form>
-                            </Card>
-                        </div>
-                    </Col>
-                    <Col md={5}></Col>
-                    <Col md={5}></Col>
-                    <Col md={4}></Col>
-                </Row>
-            </div>
-        );
-    }
-}
+  return (
+    <div className="LoginBody">
+      <Row type="flex" align="bottom">
+        <Col
+          className="gutter-row"
+          lg={2}
+          sm={24}
+        />
+        <Col className="gutter-row" lg={8} sm={24} xs={24}>
+          <div className="cardAlignment cardPadding">
+            <Card className="cardStyling" style={{ boxShadow: '5px 8px 24px 5px rgba(0, 0, 0, 0.2)' }}>
+              <h1 className="centerText">CoTeach Sign In</h1>
+              <div align="center">
+                <img src={coTeach} width="30%" />
+              </div>
+              <Form
+                name="basic"
+                type="flex"
+                justify="center"
+                align="middle"
+                validateMessages={validateMessages}
+                initialValues={{
+                  remember: true,
+                }}
+                onFinish={handleSubmit}
+              >
+                <LoginTabs changeAuthLevel={changeAuthLevel} />
+              </Form>
+            </Card>
+          </div>
+        </Col>
+        <Col md={5} />
+        <Col md={5} />
+        <Col md={4} />
+      </Row>
+    </div>
+  );
+};
 
 export default LoginPage;
