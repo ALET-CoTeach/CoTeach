2d00c918040abc5efa4234cfb5f66e42cf754a84
samsonnagamani <samson.nagamani@pm.me>
2021-08-20 14:45:09 +0100

    Fixed Upload images bug

--- client/src/components/TeacherPostCreationPage/Modals/Steps/Forms/UploadMedia.jsx
+++ client/src/components/TeacherPostCreationPage/Modals/Steps/Forms/UploadMedia.jsx
@@ -1,58 +1,21 @@
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import { Upload, Modal } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 // import ImgCrop from 'antd-img-crop'; possible to add this to crop to a sqaure (good for social media stuffs)
-
-const getBase64 = (file) => new Promise((resolve, reject) => {
-  const reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onload = () => resolve(reader.result);
-  reader.onerror = (error) => reject(error);
-});
 
 const UploadMedia = () => {
   const [previewVisible, setPreviewVisible] = useState(false);
   const [previewImage, setPreviewImage] = useState('');
   const [previewTitle, setPreviewTitle] = useState('');
   const [fileList, setFileList] = useState([]);
-  //   {
-  //     uid: '-1',
-  //     name: 'image.png',
-  //     status: 'done',
-  //      'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  //   },
-  //   {
-  //     uid: '-2',
-  //     name: 'image.png',
-  //     status: 'done',
-  //      'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  //   },
-  //   {
-  //     uid: '-3',
-  //     name: 'image.png',
-  //     status: 'done',
-  //      'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  //   },
-  //   {
-  //     uid: '-4',
-  //     name: 'image.png',
-  //     status: 'done',
-  //      'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  //   },
-  //   {
-  //     uid: '-xxx',
-  //     percent: 50,
-  //     name: 'image.png',
-  //     status: 'uploading',
-  //      'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  //   },
-  //   {
-  //     uid: '-5',
-  //     name: 'image.png',
-  //     status: 'error',
-  //   },
+
+  const getBase64 = (file) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
 
   const handleCancel = () => setPreviewVisible(false);
 
@@ -66,7 +29,7 @@ const UploadMedia = () => {
     setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
   };
 
-  const handleChange = ({ newFileList }) => setFileList({ newFileList });
+  const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
 
   const uploadButton = (
     <>
@@ -79,6 +42,7 @@ const UploadMedia = () => {
     <>
       <h2>Please Upload Media</h2>
       <p>We recommend anywhere from 1 to 3 photos/videos!</p>
+      <br />
       <Upload
         action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
         listType="picture-card"
