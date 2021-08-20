d98884265899a57988e9b255c0d9aafcbf24f8f5
Sam <sam@squail.org>
2021-08-20 15:24:49 +0100

    React Redux integration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
-import path from 'path';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, './src/assets'),
+      '@assets2': path.resolve(__dirname, './src/assets'),
     },
   },
 });
