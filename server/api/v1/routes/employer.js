ca68fd84b3c80dec9c73b2f9ddb9e416c6a07c9f
samsonnagamani <samson.nagamani@pm.me>
2021-08-22 14:21:00 +0100

    Switched employer route to async/await syntax

--- server/api/v1/routes/employer.js
+++ server/api/v1/routes/employer.js
@@ -8,30 +8,34 @@ router.post('/register', requiredRoles(['admin']), EmployerController.register);
 router.post('/signin', EmployerController.access);
 router.post('/signout', requiredRoles(['admin']), EmployerController.deauth);
 
-router.delete('/:employerId', requiredRoles(['admin']), (req, res) => {
+router.delete('/:employerId', requiredRoles(['admin']), async (req, res) => {
   const { employerId } = req.params;
 
-  EmployerController.deleteOne(employerId)
-    .then((jsonResponse) => {
-      res.status(200).json(jsonResponse);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
+  try {
+    // Delete employer by id from DB, then stores response in const
+    const jsonResponse = await EmployerController.deleteOne(employerId);
+
+    return res.status(200).json(jsonResponse);
+  } catch (err) {
+    // Send JSON error response to the 'requestee'
+    return res.status(500).json({ error: err });
+  }
 });
 
-router.put('/:employerId', requiredRoles(['admin']), (req, res) => {
+router.put('/:employerId', requiredRoles(['admin']), async (req, res) => {
   const { employerId } = req.params;
 
   const updateData = req.body;
 
-  EmployerController.updateOne(employerId, updateData)
-    .then((jsonResponse) => {
-      res.status(500).json(jsonResponse);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
+  try {
+    // Update employer by id from DB, then stores response in const
+    const jsonResponse = await EmployerController.updateOne(employerId, updateData);
+
+    return res.status(500).json(jsonResponse);
+  } catch (err) {
+    // Send JSON error response to the 'requestee'
+    return res.status(500).json({ error: err });
+  }
 });
 
 module.exports = router;
