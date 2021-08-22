ec1c55ce1d63cfaf14e22337a59aff2fbef602e8
samsonnagamani <samson.nagamani@pm.me>
2021-08-22 14:16:11 +0100

    Switched company routes to use async/await syntax

--- server/api/v1/routes/company.js
+++ server/api/v1/routes/company.js
@@ -4,52 +4,64 @@ const requiredRoles = require('../middleware/requiredRoles');
 
 const router = Router();
 
-router.post('/', requiredRoles(['admin']), (req, res) => {
+router.post('/', requiredRoles(['admin']), async (req, res) => {
+  // Request body will be processed in Controller
+  // And will be validated
   const companyData = req.body;
 
-  CompanyController.createOne(companyData)
-    .then((jsonResponse) => {
-      res.status(jsonResponse.status).json(jsonResponse);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
+  try {
+    // Create new Company in DB, and store success response in const
+    const jsonResponse = await CompanyController.createOne(companyData);
+
+    return res.status(jsonResponse.status).json(jsonResponse);
+  } catch (err) {
+    // Send JSON error response to the 'requestee'
+    return res.status(500).json({ error: err });
+  }
 });
 
-router.get('/', requiredRoles(['admin', 'slt', 'employer']), (req, res) => {
-  CompanyController.getAll()
-    .then((jsonResponse) => {
-      res.status(200).json(jsonResponse);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
+router.get('/', requiredRoles(['admin', 'slt', 'employer']), async (req, res) => {
+  try {
+    // Get all companies from DB, and store success response in const
+    const jsonResponse = await CompanyController.getAll();
+
+    return res.status(200).json(jsonResponse);
+  } catch (err) {
+    // Send JSON error response to the 'requestee'
+    return res.status(500).json({ error: err });
+  }
 });
 
-router.delete('/:companyId', requiredRoles(['admin']), (req, res) => {
+router.delete('/:companyId', requiredRoles(['admin']), async (req, res) => {
   const { companyId } = req.params;
 
-  CompanyController.deleteOne(companyId)
-    .then((jsonResponse) => {
-      res.status(200).json(jsonResponse);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
+  try {
+    // Delete a company by id in DB, and store success response in const
+    const jsonResponse = await CompanyController.deleteOne(companyId);
+
+    return res.status(200).json(jsonResponse);
+  } catch (err) {
+    // Send JSON error response to the 'requestee'
+    return res.status(500).json({ error: err });
+  }
 });
 
-router.put('/:companyId', requiredRoles(['admin']), (req, res) => {
+router.put('/:companyId', requiredRoles(['admin']), async (req, res) => {
   const { companyId } = req.params;
 
+  // Request body will be destructured in Controller method
+  // And will be validated
   const updateData = req.body;
 
-  CompanyController.updateOne(companyId, updateData)
-    .then((jsonResponse) => {
-      res.status(500).json(jsonResponse);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
+  try {
+    // Update a company by id in DB, and store success response in const
+    const jsonResponse = await CompanyController.updateOne(companyId, updateData);
+
+    return res.status(500).json(jsonResponse);
+  } catch (err) {
+    // Send JSON error response to the 'requestee'
+    return res.status(500).json({ error: err });
+  }
 });
 
 module.exports = router;
