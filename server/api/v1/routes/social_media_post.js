8a06077feae81a0c56d0de8200480f32e066f968
samsonnagamani <samson.nagamani@pm.me>
2021-08-22 15:04:12 +0100

    Switched to async/await syntax in social_media_post route

--- server/api/v1/routes/social_media_post.js
+++ server/api/v1/routes/social_media_post.js
@@ -4,53 +4,62 @@ const requiredRoles = require('../middleware/requiredRoles');
 
 const router = Router();
 
-router.post('/', requiredRoles(['admin', 'slt', 'teacher']), (req, res) => {
+router.post('/', requiredRoles(['admin', 'slt', 'teacher']), async (req, res) => {
   const socialMediaPostData = req.body;
 
-  SocialMediaPostController.createOne(socialMediaPostData)
-    .then((jsonResponse) => {
-      res.status(jsonResponse.status).json(jsonResponse);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
+  try {
+    // Create and save new SocialMediaPost from DB, then store response in const
+    const jsonResponse = await SocialMediaPostController.createOne(socialMediaPostData);
+
+    res.status(jsonResponse.status).json(jsonResponse);
+  } catch (err) {
+    // Send JSON error response to the 'requestee'
+    return res.status(500).json({ error: err });
+  }
 });
 
-router.get('/', requiredRoles(['admin', 'slt']), (req, res) => {
-  SocialMediaPostController.getAll()
-    .then((jsonResponse) => {
-      res.status(200).json(jsonResponse);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
+router.get('/', requiredRoles(['admin', 'slt']), async (req, res) => {
+  try {
+    // Get all SocialMediaPosts from DB, then store response in const
+    const jsonResponse = await SocialMediaPostController.getAll();
+
+    res.status(200).json(jsonResponse);
+  } catch (err) {
+    // Send JSON error response to the 'requestee'
+    return res.status(500).json({ error: err });
+  }
 });
 
-router.delete('/:socialMediaPostId', requiredRoles(['admin']), (req, res) => {
+router.delete('/:socialMediaPostId', requiredRoles(['admin']), async (req, res) => {
   const { socialMediaPostId } = req.params;
 
-  SocialMediaPostController.deleteOne(socialMediaPostId)
-    .then((jsonResponse) => {
-      res.status(200).json(jsonResponse);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
+  try {
+    // Delete one SocialMediaPost by id from DB, then store response in const
+    const jsonResponse = await SocialMediaPostController.deleteOne(socialMediaPostId);
+
+    res.status(200).json(jsonResponse);
+  } catch (err) {
+    // Send JSON error response to the 'requestee'
+    return res.status(500).json({ error: err });
+  }
 });
 
-router.put('/:socialMediaPostId', requiredRoles(['admin']), (req, res) => {
+router.put('/:socialMediaPostId', requiredRoles(['admin']), async (req, res) => {
   const { socialMediaPostId } = req.params;
 
+  // Request body will be destructured in Controller method
+  // And will be validated
   const updateData = req.body;
 
-  SocialMediaPostController.updateOne(socialMediaPostId, updateData)
-    .then((jsonResponse) => {
-      res.status(500).json(jsonResponse);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
+  try {
+    // Update one SocialMediaPost by id from DB, then store response in const
+    const jsonResponse = await SocialMediaPostController.updateOne(socialMediaPostId, updateData);
+
+    res.status(500).json(jsonResponse);
+  } catch (err) {
+    // Send JSON error response to the 'requestee'
+    return res.status(500).json({ error: err });
+  }
 });
 
 module.exports = router;
-
