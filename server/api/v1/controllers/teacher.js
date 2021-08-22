8df0621de0ddba1a898466c760d20c602b15d1e1
samsonnagamani <samson.nagamani@pm.me>
2021-08-22 05:05:20 +0100

    Added comments to deauth method for Teacher

--- server/api/v1/controllers/teacher.js
+++ server/api/v1/controllers/teacher.js
@@ -233,12 +233,16 @@ module.exports.access = async (req, res) => {
 
 module.exports.deauth = (req, res) => {
   try {
+    // Clear authentication headers and stored user data
     req.headers.authentication = null;
     req.teacherData = null;
+
+    // Sign out successful, send success response
     return res.status(200).json({
-      message: 'De-authenticated (Sign Out) success',
+      message: 'De-authentication (Sign Out) success',
     });
   } catch (err) {
+    // Sign out unsuccessfull, send failure response
     return res.status(401).json({
       message: 'De-authentication (Sign Out) failed',
     });
