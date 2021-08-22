6520b200363ba400d96822cec1a95b0944eaab12
samsonnagamani <samson.nagamani@pm.me>
2021-08-22 14:28:06 +0100

    Formatted requiredRoles.js

--- server/api/v1/middleware/requiredRoles.js
+++ server/api/v1/middleware/requiredRoles.js
@@ -1,27 +1,24 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (roles) => {
-  return (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
-    let isVerified = false;
+module.exports = (roles) => (req, res, next) => {
+  const token = req.headers.authorization.split(' ')[1];
+  let isVerified = false;
 
-    roles.forEach((role) => {
-      jwt.verify(token, process.env[`JWT_${role.toUpperCase()}_KEY`], (err, decoded) => {
-        if (!err) {
-          isVerified = true;
-        }
+  roles.forEach((role) => {
+    jwt.verify(token, process.env[`JWT_${role.toUpperCase()}_KEY`], (err, decoded) => {
+      if (!err) {
+        isVerified = true;
+      }
 
-        req[`${role}Data`] = decoded;
-      });
+      req[`${role}Data`] = decoded;
     });
+  });
 
-    if (isVerified) {
-      next();
-    } else {
-      return res.status(403).json({
-        message: 'Forbidden'
-      });
-    }
+  if (isVerified) {
+    next();
+  } else {
+    return res.status(403).json({
+      message: 'Forbidden',
+    });
   }
 };
-
