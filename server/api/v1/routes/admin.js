8870823d578e77eb11176e02886b5a842c29b167
samsonnagamani <samson.nagamani@pm.me>
2021-08-22 13:50:21 +0100

    Removed delete and update routes for Admin

--- server/api/v1/routes/admin.js
+++ server/api/v1/routes/admin.js
@@ -4,34 +4,8 @@ const requiredRoles = require('../middleware/requiredRoles');
 
 const router = Router();
 
-router.post('/register', requiredRoles(['admin']),  AdminController.register);
+router.post('/register', requiredRoles(['admin']), AdminController.register);
 router.post('/signin', AdminController.access);
 router.post('/signout', requiredRoles(['admin']), AdminController.deauth);
-
-router.delete('/:adminId', requiredRoles(['admin']), (req, res) => {
-  const { adminId } = req.params;
-
-  AdminController.deleteOne(adminId)
-    .then((jsonResponse) => {
-      res.status(200).json(jsonResponse);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
-});
-
-router.put('/:adminId', requiredRoles(['admin']), (req, res) => {
-    const { adminId } = req.params;
-
-    const updateData = req.body;
-
-    AdminController.updateOne(adminId, updateData)
-        .then((jsonResponse) => {
-            res.status(500).json(jsonResponse);
-        })
-        .catch((err) => {
-            res.status(500).json({ error: err });
-        });
-});
 
 module.exports = router;
