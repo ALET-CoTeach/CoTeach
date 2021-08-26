59d0b2b3c32443a4038f6c51195ddbad73a7287a
Sam <sam@squail.org>
2021-08-26 23:12:09 +0100

    Added image upload functionalitly when creating SocialMediaPost via Google Cloud Storage API

--- server/api/v1/routes/social_media_post.js
+++ server/api/v1/routes/social_media_post.js
@@ -1,22 +1,43 @@
 const { Router } = require('express');
+const { Storage } = require('@google-cloud/storage');
+const multer = require('multer');
 const SocialMediaPostController = require('../controllers/social_media_post');
 const requiredRoles = require('../middleware/requiredRoles');
+
+const upload = multer({ dest: multer.memoryStorage() });
+
 const {
   _admin, _employer, _slt, _teacher,
 } = require('../utils/UserTypes');
 
 const router = Router();
 
-router.post('/', requiredRoles([_admin, _slt, _teacher]), async (req, res) => {
+router.post('/', requiredRoles([_admin, _slt, _teacher]), multer({ storage: multer.memoryStorage() }).single('image'), async (req, res) => {
   const socialMediaPostData = req.body;
 
   if (req.user.authLevel === _teacher) {
     socialMediaPostData.status = 'pending';
   }
+  const timestamp = Date.now();
+
+  const storage = new Storage({ keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS });
+  const bucketName = 'images-aeon';
+  const bucket = storage.bucket(bucketName);
+  console.log(req.file);
+
+  const fileExt = req.file.originalname.split('.')[1];
+
+  const file = bucket.file(`${req.user._id}:${timestamp}.${fileExt}`);
+
+  console.log(req.file.buffer);
+
+  socialMediaPostData.image = file.publicUrl();
 
   try {
     // Create and save new SocialMediaPost from DB, then store response in const
     const jsonResponse = await SocialMediaPostController.createOne(socialMediaPostData);
+    if (!jsonResponse) await file.delete();
+    await file.save(req.file.buffer);
 
     res.status(jsonResponse.status).json(jsonResponse);
   } catch (err) {
