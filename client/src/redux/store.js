5f31d917cb3679caa21923069aea8e49c9658434
Sam <sam@squail.org>
2021-08-28 01:51:07 +0100

    Added employer.js RTK API query :)

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,6 +4,7 @@ import { setupListeners } from '@reduxjs/toolkit/query';
 import { activityAPI } from '../services/backendAPI/activity_request';
 import { adminAPI } from '../services/backendAPI/admin';
 import { companyAPI } from '../services/backendAPI/company';
+import { employerAPI } from '../services/backendAPI/employer';
 
 import allReducers from './reducers';
 
@@ -16,7 +17,8 @@ const store = configureStore({
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
     .concat(activityAPI.middleware)
     .concat(adminAPI.middleware)
-    .concat(companyAPI.middleware),
+    .concat(companyAPI.middleware)
+    .concat(employerAPI.middleware),
 });
 
 setupListeners(store.dispatch);
---
+++ client/src/services/backendAPI/employer.js
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const employerAPI = createApi({
+  reducerPath: 'employerAPI',
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_BASE_API_URL}/activityrequest`,
+    prepareHeaders: (headers, { getState }) => {
+      const { token } = getState().auth;
+
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    // creates a hook
+    // previxes with "use" capitalises first letter, sufexies with "Query".
+    postEmployer: builder.query({
+      query: () => '/register',
+      method: 'POST',
+    }),
+    signInEmployer: builder.query({
+      query: '/signin',
+      method: 'POST',
+    }),
+    deleteEmployer: builder.query({
+      query: ({ employerId }) => `/:${employerId}`,
+      method: 'DELETE',
+    }),
+    updateEmployer: builder.query({
+      query: ({ employerId }) => `/:${employerId}`,
+      method: 'PUT',
+    }),
+  }),
+});
+
+export const {
+  usePostEmployerQuery,
+  useSignInEmployerQuery,
+  useDeleteEmployerQuery,
+  useUpdateEmployerQuery,
+} = employerAPI;
