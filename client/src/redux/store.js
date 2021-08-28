9e12b07821994d15e4dd6412cb46eaa04df4abea
Sam <sam@squail.org>
2021-08-28 01:04:02 +0100

    Added activity_request.js RTK query :)

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,17 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 
-import { backendApi } from '../services/backend_api';
+import { activityAPI } from '../services/backendAPI/activity_request';
 
 import allReducers from './reducers';
 
 const store = configureStore({
   reducer: {
-    [backendApi.reducerPath]: backendApi.reducer,
+    [activityAPI.reducerPath]: activityAPI.reducer,
     ...allReducers,
   },
   devTools: true,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(backendApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(activityAPI.middleware),
 });
 
 setupListeners(store.dispatch);
---
+++ client/src/services/backendAPI/activity_request.js
@@ -0,0 +1,47 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const activityAPI = createApi({
+  reducerPath: 'ActivityRequestAPI',
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_BASE_API_URL}/activityrequest`,
+    prepareHeaders: (headers, { getState }) => {
+      const { token } = getState().auth;
+
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    // creates a hook
+    // previxes with "use" capitalises first letter, sufexies with "Query".
+    postActivityRequest: builder.query({
+      query: () => '/',
+      method: 'POST',
+    }),
+    getActivityRequests: builder.query({
+      query: '/available',
+    }),
+    getBookedActivityRequest: builder.query({
+      query: ({ role, id }) => `/activityrequest/booked/${role}:${id}`,
+    }),
+    deleteActivityRequest: builder.query({
+      query: ({ activityRequestId }) => `/:${activityRequestId}`,
+      method: 'DELETE',
+    }),
+    updateActivityRequest: builder.query({
+      query: ({ activityRequestId }) => `/:${activityRequestId}`,
+      method: 'PUT',
+    }),
+  }),
+});
+
+export const {
+  usePostActivityRequestQuery,
+  useGetActivityRequestsQuery,
+  useGetBookedActivityRequestQuery,
+  useDeleteActivityRequestQuery,
+  useUpdateActivityRequestQuery,
+} = activityAPI;
