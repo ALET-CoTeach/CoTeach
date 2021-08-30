5cebe7b92652d53634777cae81b3ccf33cbb13b5
samsonnagamani <samson.nagamani@pm.me>
2021-08-31 00:48:13 +0100

    Added Footer component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Layout } from 'antd';
+const { Footer } = Layout;
+
+const CustomFooter = () => (
+  <Footer style={{ textAlign: 'center' }}>&copy; CoTeach 2021</Footer>
+)
+
+export default CustomFooter;
