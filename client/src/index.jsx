369c711caca9c72aefa8dc6e1a24d0d35f98e202
Samson Nagamani <samson.nagamani@pm.me>
2021-10-03 21:45:58 +0100

    Persisting auth data in localstorage
    Implemented redux-persist to store authdata in localstorage. This way
    when a user reloads their page they wont be signed out

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,13 +4,19 @@ import 'antd/dist/antd.css';
 import './index.css';
 
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import store from './redux/store';
 
 import App from './App';
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,15 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 
 import backendAPI from '@services/backendAPI/';
 
 import allReducers from './reducers';
 
+const reducers = combineReducers({
+  ...allReducers,
+  [backendAPI.reducerPath]: backendAPI.reducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = configureStore({
-  reducer: {
-    ...allReducers,
-    [backendAPI.reducerPath]: backendAPI.reducer,
-  },
+  reducer: persistedReducer,
   devTools: true,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
     .concat(backendAPI.middleware),
