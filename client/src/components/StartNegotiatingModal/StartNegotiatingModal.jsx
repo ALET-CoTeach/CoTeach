271d408d59edc62c543837903a4d5ab54fa1d983
Samson Nagamani <samson.nagamani@pm.me>
2021-10-21 01:03:42 +0100

    Formatted StartNegotiatingModal

--- client/src/components/StartNegotiatingModal/StartNegotiatingModal.jsx
+++ client/src/components/StartNegotiatingModal/StartNegotiatingModal.jsx
@@ -3,18 +3,15 @@ import React, { useState } from 'react';
 import { Modal, Button } from 'antd';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
 
-const StartNegotiatingModal = ({ teacherEmail }) => {
-  const [visible, setVisible] = useState(false);
-
-  return (
-    <Button
-      style={{ width: 150, margin: 20 }}
-      type="primary"
-      onClick={() => Modal.confirm({
-        title: 'Confirm',
-        width: 500,
-        icon: <ExclamationCircleOutlined />,
-        content:
+const StartNegotiatingModal = ({ teacherEmail }) => (
+  <Button
+    style={{ width: 150, margin: 20 }}
+    type="primary"
+    onClick={() => Modal.confirm({
+      title: 'Confirm',
+      width: 500,
+      icon: <ExclamationCircleOutlined />,
+      content:
         (
           <>
             By starting negotiations you agree to receive emails from
@@ -22,13 +19,12 @@ const StartNegotiatingModal = ({ teacherEmail }) => {
             <span className="text-red-800">{teacherEmail}</span>
           </>
         ),
-        okText: 'I Agree',
-        cancelText: 'Cancel',
-      })}
-    >
-      Start Negotiations
-    </Button>
-  );
-};
+      okText: 'I Agree',
+      cancelText: 'Cancel',
+    })}
+  >
+    Start Negotiations
+  </Button>
+);
 
 export default StartNegotiatingModal;
