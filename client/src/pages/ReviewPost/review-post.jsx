434ccc6ac4386b233aa14225fd4236505d734312
Nicola Harrison <nicola@harrisonsworld.com>
2022-02-12 19:31:47 +0000

    Post Review page Header fixed, footer fixed.

--- client/src/pages/ReviewPost/review-post.jsx
+++ client/src/pages/ReviewPost/review-post.jsx
@@ -10,10 +10,10 @@ const { Panel } = Collapse;
 const VerifyPost = () => (
   <div>
     <Layout style={{ paddingTop: 45 }}>
-      <Layout className="site-layout">
+      <Layout className="site-layout" style={{ height: '100vh'}}>
         <Content className="addLesson-content">
           <div className="site-layout-background" style={{ margin:"1%" }}>
-            <h1 className="centerText HeadingGrey">Post Verification Page</h1>
+            <h1 className="centerText HeadingGrey" style={{ marginBottom: 20}}>Post Verification Page</h1>
             <VerifyPostTable />
           </div>
         </Content>
