570475c0fe48f1395aff20f2f704b31645b1a0b5
Samson Nagamani <samson.nagamani@pm.me>
2022-02-17 19:30:46 +0000

    Cleaned up styling

--- client/src/pages/SignIn/signin.jsx
+++ client/src/pages/SignIn/signin.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react';
-import './signin.css';
+import React from 'react';
 
 import {
   Card, Row, Col,
 } from 'antd';
-import coTeach from '@assets/CoTeach-Logo-Blue.svg';
+
+import Logo from '@assets/CoTeach-Logo-Blue.svg';
 import { SignInTabs } from '@components';
 
-
-
 const SignIn = () => (
-  <div className="LoginBody">
+  <div className="LoginBody fixed bg-fixed bg-center bg-no-repeat bg-cover">
     <Row type="flex" align="bottom">
       <Col
         className="gutter-row"
@@ -18,19 +16,16 @@ const SignIn = () => (
         sm={24}
       />
       <Col className="gutter-row" lg={8} sm={24} xs={24}>
-        <div className="cardAlignment cardPadding">
-          <Card className="cardStyling" style={{ boxShadow: '5px 8px 24px 5px rgba(0, 0, 0, 0.2)' }}>
-            <h1 className="centerText HeadingGrey" style={{ marginBottom: 20}}>CoTeach Sign In</h1>
+        <div className="lg:mt-20 mt-10">
+          <Card style={{ boxShadow: '5px 8px 24px 5px rgba(0, 0, 0, 0.2)' }}>
+            <h1 className="text-center HeadingGrey mb-5">CoTeach Sign In</h1>
             <div align="center">
-              <img src={coTeach} width="30%" alt="CoTeach Logo" />
+              <img src={Logo} width="30%" alt="CoTeach Logo" />
             </div>
             <SignInTabs />
           </Card>
         </div>
       </Col>
-      <Col md={5} />
-      <Col md={5} />
-      <Col md={4} />
     </Row>
   </div>
 );
